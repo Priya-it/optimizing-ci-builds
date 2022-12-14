9397d310c9ce653f3e2fd97e3a1a8fb8d35aabb8
other <other@ECE-A55006.austin.utexas.edu>
2022-12-13 18:28:53 -0600

    update main script

--- job_analyzer/main.py
+++ job_analyzer/main.py
@@ -16,19 +16,18 @@ def main():
             """FORKING THE PROJECT (VIA GITHUB API)"""
             """PARSING THE YAML FILE"""
             """CHANGING THE YAML FILE"""
-            forked_owner: str = "optimizing-ci-builds"
+            forked_owner: str = "Priya-it"
             repo: str = repository["name"].split("/")[1]
             print(f"\nRunning tests on {forked_owner}/{repo}")
             default_branch: str = repository["default_branch"]
-
             try:
                 sha: str = utils.retrieve_sha(owner=forked_owner, repo=repo, default_branch=default_branch)
             except ValueError as error:
                 print(error)
                 pass
 
-            # utils.add_secret(owner=forked_owner, repo=repo)
-
+            #utils.add_secret(owner=forked_owner, repo=repo)
+            
             yml_files_path = repository["Github Actions"].split(";")
             yml_files_path = [i for i in yml_files_path if i]
 
@@ -37,20 +36,23 @@ def main():
 
             for file_path in yml_files_path:
                 try:
-                    yaml_file, yaml_sha = utils.get_yaml_file("optimizing-ci-builds", repo, file_path)
+                    yaml_file, yaml_sha = utils.get_yaml_file("Priya-it", repo, file_path)
+
                 except ValueError as error:
                     print(error)
                     # continue
                     pass
+
                 configured_yaml = utils.configure_yaml_file(yaml_file, repo, file_path, time1)
                 # with open("Output.yml", "w") as text_file:
                 #     print(f"{configured_yaml}", file=text_file)
                 configured_yaml_files.append(configured_yaml)
                 yaml_shas.append(yaml_sha)
-
-            utils.retrieve_sha_ci_analyzes(forked_owner, "abc", time1)
+            #utils.retrieve_sha_ci_analyzes(forked_owner, "abc", time1)
+            utils.retrieve_sha_ci_analyzes(forked_owner, repo, time1)
             commit_sha = utils.execute(forked_owner, repo, sha, default_branch, yml_files_path, configured_yaml_files, yaml_shas)
-            # utils.check_runs(forked_owner, repo, commit_sha)
+            #print('HI=',forked_owner)
+            utils.check_runs(forked_owner, repo, commit_sha)
 
         except Exception as e:
             print(e)
