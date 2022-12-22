c926e13714d53d91ca62fd2d2ba17e196d662898
other <other@ECE-A55006.austin.utexas.edu>
2022-12-21 18:37:22 -0600

    added

--- data_process_And_cluster/find_not_accessed_file.sh
+++ data_process_And_cluster/find_not_accessed_file.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+#There are total _c_m__a,(_cm__a,cm__a),( _cma,cma),c,m
+#I will not consider _c_m__a because it is not creating, modifying and accessing any file (src/test..) at the build time
+#So, I considered 1st group(_cm__a,cm__a) and then 2nd group(__cma,cma) in the following code. It is noteworthy that all files in c and m are generated in target/
 #$1
-if [[ $1 == "" ]]; then
+
+if [[ $1 == ""  || $2 == ""  ]]; then
 	echo "plz give argument link (uses26)"
 fi
 
 uses_name=$(echo $1 | rev | cut -d'/' -f2 | rev)
-echo ${uses_name}
-rm "${uses_name}_cm_sort_Prefix_remove.csv"
-rm "tmp_site.csv"
-rm "${uses_name}_cm_a_Clustering.csv"
-rm  "contents_of_all_files_which_are_accessed.csv"
-#rm "unsort_Prefix_remove.csv"
-rm "contents_of_all_files_which_are_never_ever_accessed.csv"
-rm "cma_unsort_Prefix_remove.csv"
+#echo ${uses_name}
+#rm "${uses_name}_cm_a_sort_Prefix_remove.csv"
+#rm "${uses_name}_cm_a_Clustering.csv"
+#rm  "contents_of_all_files_which_are_accessed.csv"
+#rm "contents_of_all_files_which_are_never_ever_accessed.csv"
 
 cm__a="$1cm__a.csv"
 not_cm__a="$1cm__a.csv"
 not_c_m__a="$1_c_m__a.csv" # It will be some sort of unnecessary files
-echo ${not_cm__a}
+#echo ${not_cm__a}
 cat ${cm__a} >> "contents_of_all_files_which_are_never_ever_accessed.csv"
 cat ${not_cm__a} >> "contents_of_all_files_which_are_never_ever_accessed.csv"
 
@@ -31,21 +32,21 @@ do
 	echo ${prefix_remove} >> "cm_unsort_Prefix_remove.csv"
 done <  "contents_of_all_files_which_are_never_ever_accessed.csv"   
 
-sort "cm_unsort_Prefix_remove.csv" > "${uses_name}_cm_sort_Prefix_remove.csv"
+sort "cm_unsort_Prefix_remove.csv" > "${uses_name}_cm_a_sort_Prefix_remove.csv"
 rm  "cm_unsort_Prefix_remove.csv"
 
 #==========For Making Cluster============
-group_arr=$(cut -f1 -d'/' "${uses_name}_cm_sort_Prefix_remove.csv"  | sort | uniq)
+group_arr=$(cut -f1 -d'/' "${uses_name}_cm_a_sort_Prefix_remove.csv"  | sort | uniq)
 echo "group,count" > "${uses_name}_cm_a_Clustering.csv"
 for group in ${group_arr[@]}; do
-    count=$(grep -r "$group" "${uses_name}_cm_sort_Prefix_remove.csv" | wc -l)
+    count=$(grep -r "$group" "${uses_name}_cm_a_sort_Prefix_remove.csv" | wc -l)
     echo "$group,$count" >> "${uses_name}_cm_a_Clustering.csv"
     if [[ $group == "site" ]]; then
-        grep -r "site" "${uses_name}_cm_sort_Prefix_remove.csv"  > "tmp_site.csv"
+        grep -r "site" "${uses_name}_cm_a_sort_Prefix_remove.csv"  > "tmp_site.csv"
          group_site_arr=$(cut -f2 -d'/' "tmp_site.csv"  | sort | uniq)
          for group_site in ${group_site_arr[@]}; do
             count_site=$(grep -r "${group_site}" "tmp_site.csv" | wc -l)
-            echo ${group_site},${count_site}
+            #echo ${group_site},${count_site}
             echo "site/${group_site},${count_site}" >> "${uses_name}_cm_a_Clustering.csv"
          done
     fi
@@ -87,10 +88,16 @@ for group in ${group_arr[@]}; do
          group_site_arr=$(cut -f2 -d'/' "tmp_site.csv"  | sort | uniq)
          for group_site in ${group_site_arr[@]}; do
             count_site=$(grep -r "${group_site}" "tmp_site.csv" | wc -l)
-            echo ${group_site},${count_site}
+            #echo ${group_site},${count_site}
             echo "site/${group_site},${count_site}" >> "${uses_name}_cma_Clustering.csv"
          done
     fi
 done
+
+rm "tmp_site.csv"
+if [[ ! -d $2 ]]; then
+    mkdir $2
+fi
+mv *.csv  $2
 
 #python3 pychart_generator.py "${uses_name}_cm_Clustering.csv"
