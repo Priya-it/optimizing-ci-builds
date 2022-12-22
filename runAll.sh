65615d48318cb66dd7fe6de0c0ba9377eea6e192
other <other@ECE-A55006.austin.utexas.edu>
2022-12-21 22:03:09 -0600

    edit

--- runAll.sh
+++ runAll.sh
@@ -1,4 +1,10 @@
 #python3 data_collector/repository_collector.py
 #print('File collector')
 #python3 data_collector/file_collector.py
-python3 data_collector/content_collector.py
+#python3 data_collector/content_collector.py
+
+#Need to uncomment the following line from job_analyzer/main.py if the first time the project is run
+#utils.add_secret(owner=forked_owner, repo=repo)
+
+cd "job_analyzer/"
+python3 main.py
