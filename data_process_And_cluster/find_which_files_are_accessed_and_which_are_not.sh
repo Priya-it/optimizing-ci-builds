c5da2b42c0260c6ee1d87a4e345d977825fdf73e
other <other@ECE-A55006.austin.utexas.edu>
2022-12-22 00:34:25 -0600

    add script to get all accessed and not-accessed files together

---
+++ data_process_And_cluster/find_which_files_are_accessed_and_which_are_not.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [[ $1 == "" || $2 == "" ]]; then
+    echo "give $1 (directory name)"
+    echo "give $2 (Project name)"
+    exit
+fi
+currentDir=$(pwd)
+
+dir_arr=($(cd $1 && printf -- '%s\n' */))
+#$(find . -maxdepth 1 -type d -printf '%f\n')
+cd $1
+#echo "PWD= ${dir_arr}"
+never_accessed_file_name_array=("cm_a.csv" "c_m_a.csv" "c_m__a.csv" "cm__a.csv"  "_cm_a.csv"  "_cm__a.csv.csv"  "_c_m_a.csv" "_c_m__a.csv" )
+accessed_file_name_array=("cma.csv" "c_ma.csv" "_cma.csv"  "_c_ma.csv"  )
+
+if [[ ! -d "$currentDir/Output" ]]; then
+    mkdir "$currentDir/Output"
+fi
+
+for i in "${dir_arr[@]}"
+do
+        
+    echo "==========$i ========== $(pwd)"
+    #if [[ $i != "." ]]; then
+    for j in "${never_accessed_file_name_array[@]}"
+    do
+        #echo $i$j
+        if [ -f $i$j ]; then
+            echo "Found $i$j"
+            cat "$i$j" >> "$currentDir/Output/$2-never-accessed"
+        else 
+            echo "Not Found"
+        fi
+    done
+
+    for k in "${accessed_file_name_array[@]}"
+    do
+        if [ -f $i$k ]; then
+            echo "pwd =$(pwd)"
+            echo $i$k
+            cat "$i$k" >> "$currentDir/Output/$2-accessed"
+        fi
+    done
+    #fi
+done
